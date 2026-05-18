aac78b67f15d9a6b19104e13a3f74d528e304c2b
Tatiana <tittaty@yandex.ru>
2026-05-18 11:50:50 +0300

    объявила переменные, проверила работу const и let

---
+++ урок3/script.js
@@ -0,0 +1,16 @@
+let price = 100; // Изначальное значение
+price = 150;     // Меняем значение переменной
+console.log (price); // Выводим в консоль: 150
+
+
+let name = "Anna";
+name = "Anya";
+console.log (name); // let позволяет изменить значение переменой
+
+
+const temperature = 100;
+temperature = 150;
+console.log (temperature); // JS выдаст ошибку, потому что переменная в const не меняется
+
+
+
