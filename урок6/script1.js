289a267e615a92f92579acfa163a7324ce0f361c
Tatiana <tittaty@yandex.ru>
2026-05-22 13:04:49 +0300

    выполнила все задания

---
+++ урок6/script1.js
@@ -0,0 +1,9 @@
+const numeral = prompt('Введите число:');
+
+if (Number(numeral) > 0) {
+    console.log('Число положительное');
+} else if (Number(numeral) < 0) {
+    console.log('Число отрицательное');
+} else if (Number(numeral) == 0) {
+    console.log('Число равно нулю');
+}
