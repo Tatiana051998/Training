7c9c4ab9254b4cfe248d44b7c5e64cdce941e40d
Tatiana <tittaty@yandex.ru>
2026-05-22 14:37:38 +0300

    выполнила все задания

---
+++ урок8/script.js
@@ -0,0 +1,29 @@
+const str = 'JavaScript is fun!'
+
+console.log(str.includes `fun`);
+
+/*------------------*/
+
+let value = 0;
+
+if (!value) {
+  console.log(value);
+}
+
+/*------------------*/
+
+let firstName = "Maria";
+let lastName = "Romanova";
+let occupation = "software engineer";
+
+const message = `Hello, my name is ${firstName} ${lastName}. I am a ${occupation}.`;
+console.log(message);
+
+/*------------------*/
+
+console.log(null === undefined); 
+console.log(null == undefined);
+
+/*------------------*/
+
+console.log(1 + '1');
