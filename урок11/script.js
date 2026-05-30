5479ddbb26cc0cfbc312f92876b0aa928424cd74
Tatiana <tittaty@yandex.ru>
2026-05-30 16:32:20 +0300

    выполнила

--- урок11/script.js
+++ урок11/script.js
@@ -47,11 +47,11 @@ console.log(findMinMax([5, 2, 9, 1, 7, 3]));
 
 
 
-const bubbleSort = (arr) => { //// Создаём копию массива, чтобы не изменять оригинал
+const bubbleSort = (arr) => { // Создаём копию массива, чтобы не изменять оригинал
     const sorted = [...arr];
     
     for (let i = 0; i < sorted.length - 1; i++) {
-        for (let j = 0; j < sorted.length - 1 - i; j++) {// Если текущий элемент больше следующего - меняем их местами
+        for (let j = 0; j < sorted.length - 1 - i; j++) { // Если текущий элемент больше следующего - меняем их местами
             if (sorted[j] > sorted[j + 1]) { // Обмен значениями
                 [sorted[j], sorted[j + 1]] = [sorted[j + 1], sorted[j]];
             }
